ea01d40d2dbd993947102dfbca5bafae5a14bb34
iagoleplubo <iago.dolfini@eduvaud.ch>
2026-05-26 16:14:43 +0200

    j'ai refait l'ex metadata car j'avais rien compris la il marche bien

--- Exercice_MetaData/Calcule_hash_SHA-256.py
+++ Exercice_MetaData/Calcule_hash_SHA-256.py
@@ -1,13 +1,31 @@
+from reportlab.pdfgen import canvas
 import hashlib
 
-pdf_path = "hello_iago.pdf"
+
+# NOM DU FICHIER PDF
+
+pdf_filename = "hello_iago.pdf"
+
+
+# CREATION DU PDF
+
+c = canvas.Canvas(pdf_filename)
+
+c.drawString(100, 750, "Hello from Iago to blockchain")
+
+c.save()
+
+print("PDF créé :", pdf_filename)
+
+
+# HASH SHA-256 DU PDF
 
 sha256_hash = hashlib.sha256()
 
-with open(pdf_path, "rb") as f:
+with open(pdf_filename, "rb") as f:
     for block in iter(lambda: f.read(4096), b""):
         sha256_hash.update(block)
 
 file_hash = sha256_hash.hexdigest()
 
-print("SHA-256:", file_hash)
+print("SHA-256 :", file_hash)
